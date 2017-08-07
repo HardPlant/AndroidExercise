03f2d90143b93845998aac23086dafd2817a567e
HardPlant <abc7988se@naver.com>
2017-08-07 19:17:59 +0900

    Extract Key Value

--- OnePageButton/app/src/main/java/com/kiiren/onepagebutton/MainActivity.java
+++ OnePageButton/app/src/main/java/com/kiiren/onepagebutton/MainActivity.java
@@ -25,20 +25,38 @@ public class MainActivity extends AppCompatActivity {
         this.sendHttpRequest();
     }
     public void sendHttpRequest(){
+        /*https://code.tutsplus.com/ko/tutorials/android-from-scratch-using-rest-apis--cms-27117*/
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
                 try{
-                    URL sendMessage = new URL( "http://www.naver.com");
+                    /*HTTP 연결 생성*/
+                    URL sendMessage = new URL( "https://api.github.com/");
                     HttpsURLConnection myConnection = (HttpsURLConnection) sendMessage.openConnection();
-
+                    /*요청 헤더 추가*/
                     myConnection.setRequestProperty("User-Agent", "my-rest-app-v0.1");
-
+                    /*응답 읽기*/
                     if (myConnection.getResponseCode() == 200) {
                         InputStream responseBody = myConnection.getInputStream();
                         InputStreamReader responseBodyReader =
                                 new InputStreamReader(responseBody, "UTF-8");
                         JsonReader jsonReader = new JsonReader(responseBodyReader);
+                        /*응답에서 키 값 추출하기 (여기서는 organization_url*/
+                        jsonReader.beginObject(); // Start processing the JSON object
+                        while (jsonReader.hasNext()) { // Loop through all keys
+                            String key = jsonReader.nextName(); // Fetch the next key
+                            if (key.equals("organization_url")) { // Check if desired key
+                                // Fetch the value as a String
+                                String value = jsonReader.nextString();
+
+                                // Do something with the value
+                                // ...
+
+                                break; // Break out of the loop
+                            } else {
+                                jsonReader.skipValue(); // Skip values of other keys
+                            }
+                        }
                     } else {
                         // Error handling code goes here
                     }
