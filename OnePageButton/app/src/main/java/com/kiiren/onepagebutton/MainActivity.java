cd7b8ae52d23221f5d19c9ccc354f6632043f461
HardPlant <abc7988se@naver.com>
2017-08-07 19:09:39 +0900

    Set URL, Append Header, get Response, Parse to JSON, Exception.

--- OnePageButton/app/src/main/java/com/kiiren/onepagebutton/MainActivity.java
+++ OnePageButton/app/src/main/java/com/kiiren/onepagebutton/MainActivity.java
@@ -3,7 +3,16 @@ package com.kiiren.onepagebutton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.AsyncTask;
+import android.util.JsonReader;
 import android.view.View;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +22,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void sendMessage(View v){
-
+        this.sendHttpRequest();
     }
     public void sendHttpRequest(){
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                // All your networking logic
-                // should be here
+                try{
+                    URL sendMessage = new URL( "http://www.naver.com");
+                    HttpsURLConnection myConnection = (HttpsURLConnection) sendMessage.openConnection();
+
+                    myConnection.setRequestProperty("User-Agent", "my-rest-app-v0.1");
+
+                    if (myConnection.getResponseCode() == 200) {
+                        InputStream responseBody = myConnection.getInputStream();
+                        InputStreamReader responseBodyReader =
+                                new InputStreamReader(responseBody, "UTF-8");
+                        JsonReader jsonReader = new JsonReader(responseBodyReader);
+                    } else {
+                        // Error handling code goes here
+                    }
+                }
+                catch(MalformedURLException e){
+
+                }
+                catch(IOException e){
+
+                }
+
             }
         });
     }
