a02443bfbbd4c14a835b76d5fabe818c8a464b8c
HardPlant <abc7988se@naver.com>
2017-08-07 18:10:20 +0900

    background thread created

--- OnePageButton/app/src/main/java/com/kiiren/onepagebutton/MainActivity.java
+++ OnePageButton/app/src/main/java/com/kiiren/onepagebutton/MainActivity.java
@@ -2,6 +2,7 @@ package com.kiiren.onepagebutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.AsyncTask;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     }
     public void sendMessage(View v){
 
+    }
+    public void sendHttpRequest(){
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                // All your networking logic
+                // should be here
+            }
+        });
     }
 }
